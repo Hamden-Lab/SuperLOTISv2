17d5af413ff790018bc0db1c447f8d8374b51c89
Harrison Bradley <hmbradley1@arizona.edu>
2026-06-12 11:13:06 -0700

    Files moved

---
+++ superlotis/drivers/pfeiffer/pfeiffer.py
@@ -0,0 +1,23 @@
+import os
+from superlotis.drivers.pfeiffer.pfeiffer_controller import Controller, Pump
+from superlotis.tools.constants import PFEIFFER_IP_ADDRESS, PFEIFFER_PORT
+import socket
+import time
+
+url = f"opc.tcp://{PFEIFFER_IP_ADDRESS}:{PFEIFFER_PORT}"
+
+controller = Controller(url)
+controller.client.connect()
+
+turbo = Pump(controller, "TC80", 1)
+backing = Pump(controller, "MVP", 2)
+
+# print(PUMP_PARAMETERS["MVP"]["pumping_power"])
+
+backing.pumping_power = True
+
+time.sleep(15)
+
+backing.pumping_power = False
+
+controller.client.disconnect()
