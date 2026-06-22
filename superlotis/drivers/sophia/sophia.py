e26c58808571c50f4d628233d3bf23fd0a07c5f1
Harrison Bradley <hmbradley1@arizona.edu>
2026-06-22 13:56:35 -0700

    udated camera utilities

--- superlotis/drivers/sophia/sophia.py
+++ superlotis/drivers/sophia/sophia.py
@@ -8,15 +8,24 @@ class SOPHIA(object):
     def __init__(self):
         self.cam = PrincetonInstruments.PicamCamera(SOPHIA_SN)
 
-    def take_exposure(self, exposure_time_ms):
-        self.cam.set_attribute_value("Exposure Time", exposure_time_ms) # ms
+    def take_exposure(self):
         data = self.cam.grab(nframes=1, frame_timeout=SOPHIA_FRAME_TIMEOUT)
         return data
     
     def take_bias(self):
+        temp_exptime = self.cam.get_attribute_value("Exposure Time")
+
         self.cam.set_attribute_value("Exposure Time", 0) # ms
         data = self.cam.grab(nframes=1, frame_timeout=SOPHIA_FRAME_TIMEOUT)
+
+        self.cam.set_attribute_value("Exposure Time", temp_exptime) # ms
         return data
+
+    def get_exptime(self):
+        return self.cam.get_attribute_value("Exposure Time")
+
+    def set_exptime(self, exptime):
+        self.cam.set_attribute_value("Exposure Time", exptime)
 
     def get_temperature(self):
         return self.cam.get_attribute_value("Sensor Temperature Reading")
