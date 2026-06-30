2cabb759b7cd9d52f8c5304fb50377e54bd2b3e5
Harrison Bradley <hmbradley1@arizona.edu>
2026-06-30 16:59:21 -0700

    testing

--- superlotis/drivers/pfeiffer/pfeiffer.py
+++ superlotis/drivers/pfeiffer/pfeiffer.py
@@ -6,20 +6,31 @@ import time
 
 url = f"opc.tcp://{PFEIFFER_IP_ADDRESS}:{PFEIFFER_PORT}"
 
-controller = Controller(url)
-controller.client.connect()
+try:
+    controller = Controller(url)
+    controller.client.connect()
 
-turbo = Pump(controller, "TC80", 1)
-backing = Pump(controller, "MVP", 2)
+    turbo = Pump(controller, "TC80", 1)
+    backing = Pump(controller, "MVP", 2)
 
-# print(PUMP_PARAMETERS["MVP"]["pumping_power"])
+    backing.pumping_power = False
 
-backing.pumping_power = False
+    while True:
 
-print(turbo.heating)
+        print(f"Backing temperature: {backing.temperature}")
+
+        print(f"Turbo power stage temperature: {turbo.temp_power_stage}")
+        print(f"Turbo electronics temperature: {turbo.temp_electronics}")
+        print(f"Turbo lower temperature: {turbo.temp_lower}")
+
+        print(f"Turbo pump speed: {turbo.actual_speed}")
+
+        print(f"Backing pump error: {backing.error_code}")
+        print(f"Turbo pump error: {turbo.error_code}")
+
+        time.sleep(5)
+
+except KeyboardInterrupt:
 
 # time.sleep(15)
-
-# backing.pumping_power = False
-
-controller.client.disconnect()
+    controller.client.disconnect()
