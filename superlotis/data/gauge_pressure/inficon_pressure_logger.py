a11f1731b44a4cfc7f01853725e82ec7ea429108
SUPERLOTIS <51495609+Kelian98@users.noreply.github.com>
2026-07-31 16:41:13 -0700

    Logger script file for inficon gauges

--- superlotis/data/gauge_pressure/inficon_pressure_logger.py
+++ superlotis/data/gauge_pressure/inficon_pressure_logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Dict
 
-ROOT_DIR = Path(__file__).resolve().parents[2]
+ROOT_DIR = Path(__file__).resolve().parents[3]
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 
