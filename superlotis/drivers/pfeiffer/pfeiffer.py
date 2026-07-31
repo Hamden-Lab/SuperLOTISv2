4801fed045aa5d6cae23dac444eec8c1249d8b9e
SUPERLOTIS <51495609+Kelian98@users.noreply.github.com>
2026-07-31 16:41:53 -0700

    More prints for the pumps

--- superlotis/drivers/pfeiffer/pfeiffer.py
+++ superlotis/drivers/pfeiffer/pfeiffer.py
@@ -13,6 +13,8 @@ try:
     turbo = Pump(controller, "TC80", 1)
     backing = Pump(controller, "MVP", 2)
 
+    backing.pumping_power = True
+
     turbo.pumping_power = True
 
     while True:
@@ -22,8 +24,17 @@ try:
         print(f"Turbo power stage temperature: {turbo.temp_power_stage}")
         print(f"Turbo electronics temperature: {turbo.temp_electronics}")
         print(f"Turbo lower temperature: {turbo.temp_lower}")
-
         print(f"Turbo pump speed: {turbo.actual_speed}")
+        print(f"Turbo driving voltage (V): {turbo.drv_voltage:.2f}")
+        print(f"Turbo driving current (A): {turbo.drv_current:.2f}")
+        print(f"Turbo driving power (W): {turbo.drv_power}")
+        print(f"Turbo rotor temperature (C): {turbo.temp_rotor}")
+
+        print(f"Turbo temperature management mode: {turbo.cfg_acc_a1}")
+
+        print(f"Configuration accessory connection A1: {turbo.cfg_acc_a1}")
+
+        
 
         print(f"Backing pump error: {backing.error_code}")
         print(f"Turbo pump error: {turbo.error_code}")
