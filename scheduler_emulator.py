e2b7517006ee97ced00a348120b3b1c8b4aed4d2
SUPERLOTIS <51495609+Kelian98@users.noreply.github.com>
2026-07-31 16:38:01 -0700

    Comment out off port 2 to avoid mistake

--- scheduler_emulator.py
+++ scheduler_emulator.py
@@ -35,10 +35,12 @@ PORT = TEST_SCHEDULER_SERVER_PORT
 
 data_lock = Lock()
 
-data = """now 0 LYMAN PDU poweroff 2
-now 15 LYMAN PDU poweron 2
-now 30 LYMAN PDU get status 2
-"""
+# data = """now 0 LYMAN PDU poweroff 2
+# now 15 LYMAN PDU poweron 2
+# now 30 LYMAN PDU get status 2
+# """
+
+data = """now 30 LYMAN PDU get status 2"""
 
 # =========================================================
 # COMMAND PROCESSING
